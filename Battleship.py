578b9608c727b5f321d232e6f889b8a1f7ab71dc
Fidel-Willis <lucaskohorst25@gmail.com>
2016-02-21 10:20:31 -0500

    Fixed Function
    -- Fixed the game function so that the board would be printed after the game is replayed.

--- Battleship.py
+++ Battleship.py
@@ -1,33 +1,33 @@
 from random import randint
 
-board = []
-
-for x in range(5):
-    board.append(["O"] * 5)
-
-def print_board(board):
-    for row in board:
-        print " ".join(row)
-
-print "Let's play Battleship!"
-print "You have 4 turns"
-print "Misses will be shown as 'X' on the board"
-print_board(board)
-
-def random_row(board):
-    return randint(0, len(board[0]) - 1)
-
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-# print ship_row <-Prints ship row
-# print ship_col <-Prints ship column
-
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
 def game():
+    board = []
+    
+    for x in range(5):
+        board.append(["O"] * 5)
+    
+    def print_board(board):
+        for row in board:
+            print " ".join(row)
+    
+    print "Let's play Battleship!"
+    print "You have 4 turns"
+    print "Misses will be shown as 'X' on the board"
+    print_board(board)
+    
+    def random_row(board):
+        return randint(0, len(board[0]) - 1)
+    
+    def random_col(board):
+        return randint(0, len(board[0]) - 1)
+    
+    ship_row = random_row(board)
+    ship_col = random_col(board)
+    # print ship_row <-Prints ship row
+    # print ship_col <-Prints ship column
+    
+    # Everything from here on should go in your for loop!
+    # Be sure to indent four spaces!
     for turn in range(4):
         print "Turn", turn +1
         guess_row = int(raw_input("Guess Row:"))
