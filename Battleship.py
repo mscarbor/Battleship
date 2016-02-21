1dcda0457794a8c1306b7f9ad67b666bf3e5f41a
Fidel-Willis <lucaskohorst25@gmail.com>
2016-02-21 10:00:48 -0500

    Changed the game into a function...
    -- Added the game function, so that the game could be iterated again
       -- Added the replay if/else statement, if user wants to play again

--- Battleship.py
+++ Battleship.py
@@ -27,26 +27,34 @@ ship_col = random_col(board)
 
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
-for turn in range(4):
-    print "Turn", turn +1
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
-
-    if guess_row == ship_row and guess_col == ship_col:
-        print "Congratulations! You sunk my battleship!"
-        break
-    else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or     guess_col > 4):
-            print "Oops, that's not even in the ocean."
-        elif(board[guess_row][guess_col] == "X"):
-            print "You guessed that one already."
+def game():
+    for turn in range(4):
+        print "Turn", turn +1
+        guess_row = int(raw_input("Guess Row:"))
+        guess_col = int(raw_input("Guess Col:"))
+    
+        if guess_row == ship_row and guess_col == ship_col:
+            print "Congratulations! You sunk my battleship!"
+            break
         else:
-            if turn == 3:
-                print "Game Over"
-                board[guess_row][guess_col] = "X"
-                board[ship_row][ship_col]="B"
+            if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or     guess_col > 4):
+                print "Oops, that's not even in the ocean."
+            elif(board[guess_row][guess_col] == "X"):
+                print "You guessed that one already."
             else:
-                print "You missed my battleship!"
-                board[guess_row][guess_col] = "X"
-        # Print (turn + 1) here!
-            print_board(board)
+                if turn == 3:
+                    print "Game Over"
+                    board[guess_row][guess_col] = "X"
+                    board[ship_row][ship_col]="B"
+                else:
+                    print "You missed my battleship!"
+                    board[guess_row][guess_col] = "X"
+            # Print (turn + 1) here!
+                print_board(board)
+    re_play = raw_input("Want to play agian?")
+    if (re_play.lower()) == "yes":
+        print "Okay, good luck!"
+        game()
+    else:
+        print "Alright"
+game()      
